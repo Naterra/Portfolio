010ece405d076ab58843b90573b6f189b14cdff9
Naterra <natalinyweb@hotmail.com>
2018-01-27 03:37:22 -0500

    update prod config

--- config/prod.js
+++ config/prod.js
@@ -1,12 +1,12 @@
 module.exports = {
     "apiPrefix": "http://localhost:3001/",
     "serverPort": "3001",
-    "cookieKey":"",
+    "cookieKey":  process.env.cookieKey,
     "mongo_URL":  process.env.mongo_URL,
     "googleAuth":{
-        "googleClientId" :"",
-        "googleClientSecret" :"",
-        "callbackURL":""
+        "googleClientId" : process.env.googleClientId,
+        "googleClientSecret" : process.env.googleClientSecret,
+        "callbackURL": process.env.callbackURL
     }
 };
 
