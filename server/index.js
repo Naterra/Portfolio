7e8891f31154f2db2ddb3de1ca839b2e273012b9
Naterra <natalinyweb@hotmail.com>
2018-02-09 22:29:54 -0500

    update

--- server/index.js
+++ server/index.js
@@ -61,7 +61,7 @@ if (process.env.NODE_ENV === 'production') {
 	const path = require('path');
 	app.get('*', (req, res) => {
 	    console.log('__dirname', __dirname);
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+		res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
 	});
 }
 
